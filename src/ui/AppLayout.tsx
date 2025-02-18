7d6fcd5ad647e356f755bd4ffa8d93ee5dd70bb7
aryman <amandaryman@gmail.com>
2025-02-17 21:09:57 -0800

    fix build error

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -1,4 +1,5 @@
-import { Outlet, useNavigation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+// import { Outlet, useNavigation } from "react-router-dom";
 import Header from './Header';
 // import Footer from "./Footer";
 // import Loader from "./Loader";
